c75b4d4483f5161790d65c6134744eee27984373
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-07-10 11:38:26 +0300

    fixed error.

--- LeetCode_sol/src/Maximum_Depth_of_Binary_Tree_104.java
+++ LeetCode_sol/src/Maximum_Depth_of_Binary_Tree_104.java
@@ -1,26 +1,16 @@
-import javax.swing.tree.TreeNode;
 
 public class Maximum_Depth_of_Binary_Tree_104 {
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode() {}
-     *     TreeNode(int val) { this.val = val; }
-     *     TreeNode(int val, TreeNode left, TreeNode right) {
-     *         this.val = val;
-     *         this.left = left;
-     *         this.right = right;
-     *     }
-     * }
-     */
 
-        public int maxDepth(TreeNode root) {
-            if ( root == null) return 0;
-            return 1+ Math.max(maxDepth(root.left),maxDepth(root.right));
-
-        }
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
     }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+
+    }
+}
 
