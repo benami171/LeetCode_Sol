bab67278b9cbf553f749598d83c9509713280231
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-07-12 15:42:43 +0300

    Update Remove_Element_27.java

--- LeetCode_sol/src/Remove_Element_27.java
+++ LeetCode_sol/src/Remove_Element_27.java
@@ -1,17 +1,18 @@
 class Solution {
+    
     public int removeElement(int[] nums, int val) {
         int final_size = 0;
         if (nums.length == 0) {
             return 0;
         }
+        
         for ( int i = 0 ; i < nums.length ; i ++ ) {
             if ( nums[i] != val){
-
                 nums[final_size] = nums[i];
                 final_size++;
             }
         }
+    
         return final_size;
     }
-
 }
