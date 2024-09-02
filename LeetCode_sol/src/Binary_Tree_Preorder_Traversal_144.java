e524f0ac293c28cac40d15fea4a330eff192bc74
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-02 20:15:57 +0300

    Edited some solutions and fixed some compilation errors.

--- LeetCode_sol/src/Binary_Tree_Preorder_Traversal_144.java
+++ LeetCode_sol/src/Binary_Tree_Preorder_Traversal_144.java
@@ -1,20 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
-class Solution {
+public class Binary_Tree_Preorder_Traversal_144 {
 
     public List<Integer> preorderTraversal(TreeNode root) {
         return dfs(root, new ArrayList());
