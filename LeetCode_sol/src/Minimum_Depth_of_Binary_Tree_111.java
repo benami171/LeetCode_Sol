18154dde50535ff12cc6c2402041e2d7e654d2e3
Gal Ben Ami <benami171@gmail.com>
2024-09-02 19:49:23 +0300

    Update Minimum_Depth_of_Binary_Tree_111.java

--- LeetCode_sol/src/Minimum_Depth_of_Binary_Tree_111.java
+++ LeetCode_sol/src/Minimum_Depth_of_Binary_Tree_111.java
@@ -15,22 +15,16 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-
-        if ( root == null){
-            return 0;
-        }
-
-
+        if (root == null){return 0;}
         if (root.left == null && root.right == null){
             return 1;
         }
-        if ( root.left == null){
+        if (root.left == null){
             return 1 + minDepth(root.right);
         }
-        if ( root.right == null){
+        if (root.right == null){
             return 1 + minDepth(root.left);
         }
-
         return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
     }
 }
