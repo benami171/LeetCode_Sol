047d3f2909c47f2738348ce71645b02623692eaa
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-09 19:11:40 +0300

    fixed some compilation errors.

--- LeetCode_sol/src/Rotate_String_796.java
+++ LeetCode_sol/src/Rotate_String_796.java
@@ -1,4 +1,4 @@
-class Solution {
+class Rotate_String_796 {
     public boolean rotateString(String s, String goal) {
         String doubleS = s + s;
         return s.length() == goal.length() && doubleS.contains(goal);
