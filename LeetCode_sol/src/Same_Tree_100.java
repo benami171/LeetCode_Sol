098db940cf3f0f31ae7301a2e82a601b7e7e18df
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-09 19:11:50 +0300

    fixed some compilation errors.

--- LeetCode_sol/src/Same_Tree_100.java
+++ LeetCode_sol/src/Same_Tree_100.java
@@ -13,7 +13,7 @@
  *     }
  * }
  */
-class Solution {
+class Same_Tree_100 {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if ( p == null && q == null){
             return true;
