79706be129bfe8da190e7c6e5c4187deb898b501
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-09 19:12:08 +0300

    fixed some compilation errors.

--- LeetCode_sol/src/Shuffle_String_1528.java
+++ LeetCode_sol/src/Shuffle_String_1528.java
@@ -1,4 +1,4 @@
-class Solution {
+class Shuffle_String_1528 {
     public String restoreString(String s, int[] indices) {
         StringBuilder ans = new StringBuilder(s);
         for (int i = 0; i < indices.length; i++) {
