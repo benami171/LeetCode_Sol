eb1b7b68beb1a512c511e6d7c0279f2e784520ef
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-13 18:19:00 +0300

    wrote it more elegantly.

--- LeetCode_sol/src/Find_Target_Indices_After_Sorting_Array_2089.java
+++ LeetCode_sol/src/Find_Target_Indices_After_Sorting_Array_2089.java
@@ -2,29 +2,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Find_Target_Indices_After_Sorting_Array_2089 {
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
     public List<Integer> targetIndices(int[] nums, int target) {
 
         List<Integer> ans = new ArrayList<>();
 
-        for ( int i = 0 ; i < nums.length-1 ; i ++ ) {
-            for ( int j = 0  ; j < nums.length-i-1 ; j ++ ) {
-                if (nums[j] > nums[j+1] ){
-                    swap(nums,j,j+1);
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums, j, j + 1);
                 }
             }
         }
 
-        for( int i = 0 ; i < nums.length ; i ++ ){
-            if ( nums[i] == target){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
                 ans.add(i);
             }
         }
         return ans;
-    }
-
-    private static void swap (int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
