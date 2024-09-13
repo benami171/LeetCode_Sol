cda42b21cfeabd2d310c3a7a295ae0bf445968d2
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-13 19:19:36 +0300

    some refactoring

--- LeetCode_sol/src/Valid_Parentheses_20.java
+++ LeetCode_sol/src/Valid_Parentheses_20.java
@@ -4,18 +4,14 @@ class Valid_Parentheses_20 {
     public static boolean isValid(String s) {
         if (s.length() < 2) return false;
         Stack<Character> stack = new Stack<>();
-        for ( int i = 0 ; i < s.length() ; i++) {
+        for (int i = 0; i < s.length(); i++) {
 
-            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[' ){
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
                 stack.push(s.charAt(i));
-            } else if ( s.charAt(i) == ')' && !stack.isEmpty() && stack.peek() == '(')
-                stack.pop();
-            else if ( s.charAt(i) == ']' && !stack.isEmpty() && stack.peek() == '[')
-                stack.pop();
-            else if ( s.charAt(i) == '}' && !stack.isEmpty() && stack.peek() == '{')
-                stack.pop();
-            else
-                return false;
+            } else if (s.charAt(i) == ')' && !stack.isEmpty() && stack.peek() == '(') stack.pop();
+            else if (s.charAt(i) == ']' && !stack.isEmpty() && stack.peek() == '[') stack.pop();
+            else if (s.charAt(i) == '}' && !stack.isEmpty() && stack.peek() == '{') stack.pop();
+            else return false;
         }
         return stack.isEmpty();
 
