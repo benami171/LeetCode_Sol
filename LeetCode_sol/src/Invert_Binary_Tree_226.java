318941432017820c974fdaa563ae95bc7c788f2e
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-13 18:21:46 +0300

    removed redundant code line.

--- LeetCode_sol/src/Invert_Binary_Tree_226.java
+++ LeetCode_sol/src/Invert_Binary_Tree_226.java
@@ -1,30 +1,29 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Invert_Binary_Tree_226 {
     public TreeNode invertTree(TreeNode root) {
-        if ( root == null){
+        if (root == null) {
             return null;
         }
-        if ( root.left == null && root.right == null){
+        if (root.left == null && root.right == null) {
             return root;
         }
 
         TreeNode leftNode = invertTree(root.left);
-        TreeNode rightNode = invertTree(root.right);
-        root.left = rightNode;
+        root.left = invertTree(root.right);
         root.right = leftNode;
 
         return root;
