c0f742f63c32c8cee669fed40182b15fe3d5b457
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-19 21:13:47 +0300

    added buddy strings question.

--- LeetCode_sol/src/Buddy_Strings_859.java
+++ LeetCode_sol/src/Buddy_Strings_859.java
@@ -1,9 +1,19 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Buddy_Strings_859 {
     public boolean buddyStrings(String s, String goal) {
         if (s.length() != goal.length()) return false;
+
+
+        if (s.equals(goal)){
+            HashSet<Character> unique_c = new HashSet<>();
+            for (char c: s.toCharArray()){
+                unique_c.add(c);
+            }
+            return unique_c.size() < s.length();
+        }
         List<Integer> diff = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) != goal.charAt(i)) {
