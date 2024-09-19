eb12b1471f9773e7b2c111888c8b439c1db20180
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-19 20:54:20 +0300

    added buddy strings question.

--- LeetCode_sol/src/Buddy_Strings_859.java
+++ LeetCode_sol/src/Buddy_Strings_859.java
@@ -7,9 +7,18 @@ public class Buddy_Strings_859 {
         List<Integer> diffIndexes = new ArrayList<>();
         for (int i = 0; i < A.length(); i++) {
             if (A.charAt(i) != B.charAt(i)) {
-
+                diffIndexes.add(i);
             }
-        }
+            if (diffIndexes.size() == 2){
+                if ( A.charAt(diffIndexes.get(0)) == B.charAt(diffIndexes.get(1)) && A.charAt(diffIndexes.get(1) == B.charAt(diffIndexes.getFirst())){
 
+                }
+                // aaab  aggb
+                // aafb  agab
+                // 1,2
+            }
+
+        }
+        return true;
     }
 }
