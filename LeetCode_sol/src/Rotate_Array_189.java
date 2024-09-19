9b8cbf276979a6a3fb9989955e76fee190179ae4
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-19 21:35:56 +0300

    added Rotate Array solution.

--- LeetCode_sol/src/Rotate_Array_189.java
+++ LeetCode_sol/src/Rotate_Array_189.java
@@ -1,2 +1,21 @@
+import java.util.Arrays;
+
+
 public class Rotate_Array_189 {
+    public void rotate (int[] arr,int k){
+        k = k % arr.length;
+        reverse(arr,0,arr.length);
+        reverse(arr,0,k);
+        reverse(arr,k,arr.length);
+    }
+
+    private void reverse(int[] arr,int start, int end){
+        while (start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = arr[temp];
+            start++;
+            end--;
+        }
+    }
 }
