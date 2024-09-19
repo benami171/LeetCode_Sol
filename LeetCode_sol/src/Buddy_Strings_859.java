075bef871ebb0101bba3fc1308617bddea177a8c
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-19 20:41:23 +0300

    added buddy strings question.

--- LeetCode_sol/src/Buddy_Strings_859.java
+++ LeetCode_sol/src/Buddy_Strings_859.java
@@ -1,2 +1,16 @@
 public class Buddy_Strings_859 {
+    public boolean buddyStrings(String A, String B) {
+        if (A.length() != B.length()) return false;
+        for (int i = 0 ; i < A.length() ; i++){
+            if (A.charAt(i) != B.charAt(i)) {
+                if (i == A.length() - 1) return false;
+                for (int j = i+1 ; j < A.length() ; j++){
+                    if(A.charAt(j) != B.charAt(j)) {
+                        char temp = A.
+
+                    }
+                }
+            }
+        }
+    }
 }
