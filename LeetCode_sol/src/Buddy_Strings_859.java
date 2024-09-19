43aa10e01084741a615e31f275e5b209e16d07c8
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-19 21:01:27 +0300

    added buddy strings question.

--- LeetCode_sol/src/Buddy_Strings_859.java
+++ LeetCode_sol/src/Buddy_Strings_859.java
@@ -2,23 +2,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Buddy_Strings_859 {
-    public boolean buddyStrings(String A, String B) {
-        if (A.length() != B.length()) return false;
+    public boolean buddyStrings(String s, String goal) {
+        if (s.length() != goal.length()) return false;
         List<Integer> diff = new ArrayList<>();
-        for (int i = 0; i < A.length(); i++) {
-            if (A.charAt(i) != B.charAt(i)) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != goal.charAt(i)) {
                 diff.add(i);
             }
             if (diff.size() == 2){
-                if ( A.charAt(diff.get(0)) == B.charAt(diff.get(1)) && A.charAt(diff.get(1)) == B.charAt(diff.getFirst())){
+                if ( s.charAt(diff.get(0)) == goal.charAt(diff.get(1)) && s.charAt(diff.get(1)) == goal.charAt(diff.getFirst())){
                     return true;
                 }
             }
             if (diff.isEmpty()) {
-                if(A.length() == 2 ){
-                    if (A.charAt(0) == A.charAt(1)) return true;
+                if(s.length() == 2 ){
+                    if (s.charAt(0) == s.charAt(1)) return true;
                 }
-                if (A.length() > 2) {
+                if (s.length() > 2) {
                     return true;
                 }
             }
