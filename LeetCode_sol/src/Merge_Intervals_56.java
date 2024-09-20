1f98aea2788969915a92a98d9d2778b0f7c63c22
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-20 23:50:00 +0300

    added solution to problem 56.

--- LeetCode_sol/src/Merge_Intervals_56.java
+++ LeetCode_sol/src/Merge_Intervals_56.java
@@ -1,19 +1,19 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Merge_Intervals_56 {
     public int[][] merge(int[][] intervals){
         if (intervals.length <= 1) return intervals;
 
-        Arrays.sort(intervals,(arr1,arr2) -> Integer.compare(arr1[0],arr2[0]));
+        Arrays.sort(intervals, Comparator.comparingInt(arr -> arr[0]));
 
         List<int[]> output_Arr = new ArrayList<>();
         int[] current_interval = intervals[0];
         output_Arr.add(current_interval);
 
         for (int[] interval: intervals) {
-            int current_begin = current_interval[0];
             int current_end = current_interval[1];
             int next_begin = interval[0];
             int next_end = interval[1];
