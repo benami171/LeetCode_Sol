5b72d4cabf3087f7e1dc858a2a46b9545186615b
Ben Ami <Benami171@gmail.com>
2024-09-22 23:54:45 +0300

    Added Sort List problem.

--- LeetCode_sol/src/Sort_List_148.java
+++ LeetCode_sol/src/Sort_List_148.java
@@ -22,6 +22,21 @@ public class Sort_List_148 {
     }
 
     public ListNode merge(ListNode head1, ListNode head2){
+        ListNode sorted_temp = new ListNode(0);
+        ListNode current_node = sorted_temp;
+
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
+                current_node.next = head1;
+                head1 = head1.next;
+            } else {
+                current_node.next = head2;
+                head2 = head2.next;
+            }
+            current_node = current_node.next;
+        }
+
+        return sorted_temp.next;
 
     }
 }
