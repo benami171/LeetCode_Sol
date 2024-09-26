3af7042285b4f135436c614e77f5d76eb405873f
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-26 16:23:58 +0300

    added question 11

--- LeetCode_sol/src/Container_With_Most_Water_11.java
+++ LeetCode_sol/src/Container_With_Most_Water_11.java
@@ -1,6 +1,18 @@
 public class Container_With_Most_Water_11 {
     public int maxArea(int[] height) {
-    int start_p =0;
-    int end_p =0
+    int start_p = 0;
+    int end_p = 0;
+    int max = 0;
+
+    while (start_p < end_p) {
+        if (height[start_p] < height[end_p]){
+            max = Math.max(max, height[start_p] * (end_p - start_p));
+            start_p++;
+        } else {
+            max = Math.max(max, height[end_p] * (end_p - start_p));
+            end_p--;
+        }
+    }
+    return max;
     }
 }
