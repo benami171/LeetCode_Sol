80e21f408a519887ed4e65954161dafefd5cdf31
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-29 11:07:15 +0300

    working on problem 206

--- LeetCode_sol/src/Reverse_Linked_List_206.java
+++ LeetCode_sol/src/Reverse_Linked_List_206.java
@@ -3,5 +3,12 @@ public class Reverse_Linked_List_206 {
     public ListNode reverseList(ListNode head) {
     ListNode prev = null;
 
+    while(head != null) {
+        ListNode next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+    }
+
     }
 }
