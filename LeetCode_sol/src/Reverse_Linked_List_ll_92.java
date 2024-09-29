fd340b9be5ff10d7b810c0a14f6263e9bdb09aac
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-09-29 11:30:32 +0300

    working on problem 92.

--- LeetCode_sol/src/Reverse_Linked_List_ll_92.java
+++ LeetCode_sol/src/Reverse_Linked_List_ll_92.java
@@ -1,6 +1,29 @@
 public class Reverse_Linked_List_ll_92 {
 
     public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null) return null;
+        ListNode prev = null;
+        ListNode curr = head;
+        while (m > 1) {
+            prev = curr;
+            curr = curr.next;
+            m--;
+            n--;
+        }
+        // now that we're here, the n is the number of indexes from the current point forward
+        // that we need to reverse, say we got m=2 and n=4, once we got to position 2, we have
+        // 3 numbers including the current one to reverse.
+        ListNode connector = prev;
+        ListNode tail = curr; // will use to connect the reverse tail
 
+        while (n > 0) {
+            n--;
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev
+        }
     }
 }
+// 1->2->3->4->5->NULL
+// prev = 1, curr = 2, connector = 1, tail = 2
+//
