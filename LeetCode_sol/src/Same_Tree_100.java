360fa2daba15246333a365dacf25f8ae7fbebe77
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2025-05-17 14:18:21 +0300

    Changed solution to problem 100 ,I like the new style better.

--- LeetCode_sol/src/Same_Tree_100.java
+++ LeetCode_sol/src/Same_Tree_100.java
@@ -1,18 +1,14 @@
 
 class Same_Tree_100 {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if ( p == null && q == null){
-            return true;
-        }
-        if ( p == null || q == null){
-            return false;
-        }
+        return dfs(p,q);
+    }
 
-        if ( q.val != p.val) {
-            return false;
-        }
-
-        return isSameTree(p.left , q.left) && isSameTree(p.right ,q.right);
+    private boolean dfs(TreeNode p, TreeNode q){
+        if (q == null && p == null) return true;
+        if (p == null || q == null) return false;
+        if (q.val != p.val) return false;
+        return (dfs(p.left,q.left) && dfs(p.right,q.right));
     }
 }
 
