a95f52659e4aceb57ffa9a2a8cf814ded9156ecf
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2025-05-17 13:32:23 +0300

    added solution to problems 543.

--- LeetCode_sol/src/Diameter_Of_Binary_Tree_543.java
+++ LeetCode_sol/src/Diameter_Of_Binary_Tree_543.java
@@ -1,7 +1,6 @@
 public class Diameter_Of_Binary_Tree_543 {
 
     public int diameterOfBinaryTree(TreeNode root) {
-        if (root == null) return 0;
         int[] max = new int[1];
         dfs(root, max);
         return max[0];
@@ -9,22 +8,25 @@ public class Diameter_Of_Binary_Tree_543 {
 
     private int dfs(TreeNode node, int[] max) {
         if (node == null) return 0;
+
         int left = dfs(node.left, max);
         int right = dfs(node.right, max);
+
         max[0] = Math.max(max[0], left + right);
-        return Math.max(left, right) + 1;
+        int currReturn = Math.max(left, right) + 1;
+        return currReturn;
     }
 
+
     public static void main(String[] args) {
-//        Diameter_Of_Binary_Tree_543 obj = new Diameter_Of_Binary_Tree_543();
-//        TreeNode root = new TreeNode(1);
-//        root.right = new TreeNode(3);
-//        root.right.right = new TreeNode(5);
-//        root.right.right.left = new TreeNode(7);
-//        root.right.left = new TreeNode(4);
-//        root.right.left.right = new TreeNode(6);
-//        root.right.left.right.right = new TreeNode(10);
-//        System.out.println(obj.diameterOfBinaryTree(root)); // Output: 3
+        Diameter_Of_Binary_Tree_543 obj = new Diameter_Of_Binary_Tree_543();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.right = new TreeNode(5);
+        root.left.right.right = new TreeNode(6);
+        root.left.left = new TreeNode(3);
+        root.left.left.left = new TreeNode(4);
+        System.out.println(obj.diameterOfBinaryTree(root)); // Output: 4
     }
 
 }
